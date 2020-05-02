e23ace9081ba97833df7f6ab3954ac0989ba32c9
Weimin Ding <dingweimin@tcl.com>
2020-05-01 18:18:11 -0700

    add onActivityResult

--- app/src/main/java/com/rmondjone/androidcamerademo/MainActivity.java
+++ app/src/main/java/com/rmondjone/androidcamerademo/MainActivity.java
@@ -1,12 +1,27 @@
 package com.rmondjone.androidcamerademo;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.rmondjone.camera.CameraActivity;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static  final int TAKE_PHOTO = 2005;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +35,25 @@ public class MainActivity extends AppCompatActivity {
      * 作者：郭翰林
      */
     public void gotoCamare(View view) {
-        CameraActivity.startMe(this, 2005, CameraActivity.MongolianLayerType.IDCARD_POSITIVE);
+        CameraActivity.startMe(this,  TAKE_PHOTO, CameraActivity.MongolianLayerType.IDCARD_POSITIVE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode== TAKE_PHOTO && resultCode == RESULT_OK)
+        {
+            String imagePath = data.getStringExtra(CameraActivity.KEY_IMAGE_PATH);
+            File image = new File(imagePath);
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(),bmOptions);
+            Log.e("Alading", "Get a bitmap size =  " +bitmap.getWidth() + "," +bitmap.getHeight());
+
+        }
+    }
+
+
 
 }
